e660f3803f4faa82681b1e020d2ea5be7faf272c
eng-alimallouhe <engalimallouhe@gmail.com>
2025-05-05 14:58:14 +0300

    build(domain): Added IEmail Service to send emails by MailKit package

--- LMS/LMS.Domain/Entites/Financial/LoyaltyLevel.cs
+++ LMS/LMS.Domain/Entites/Financial/LoyaltyLevel.cs
@@ -1,4 +1,4 @@
-﻿using LMS.Domain.Entities.Users;
+﻿using LMS.Domain.Entites.Users;
 
 namespace LMS.Domain.Entities.Financial
 {
@@ -7,22 +7,26 @@ namespace LMS.Domain.Entities.Financial
         //Primary Key:
         public Guid LevelId { get; set; }
 
+
         public string LevelName { get; set; } = string.Empty;
         public int RequiredPoints { get; set; }
         public decimal DiscountPercentage { get; set; }
         public string LevelDescription { get; set; } = string.Empty;
-
         public decimal PointPerDolar { get; set; }
+
 
         //soft delete: 
         public bool IsActive { get; set; }
+
 
         //Timestamp:
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
+
         //navigation property:
         public ICollection<Customer> Customers { get; set; }
+
 
         public LoyaltyLevel()
         {
